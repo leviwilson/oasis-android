685bd2ebdb65edeccc728ebfbdc1bc7511c4f281
Levi Wilson <levi@leviwilson.com>
2012-02-04 14:57:30 -0500

    added ActionBarSherlock submodule

---
+++ src/com/oasis/HomeActivity.java
@@ -0,0 +1,15 @@
+
+package com.oasis;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+public class HomeActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.splash);
+	}
+}
