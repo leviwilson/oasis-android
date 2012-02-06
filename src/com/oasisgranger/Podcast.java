71cc18c6dc9655d5461bc51e9267efe3eeb8f800
Levi Wilson <levi@leviwilson.com>
2012-02-06 00:44:06 -0500

    updated feed url

--- src/com/oasisgranger/Podcast.java
+++ src/com/oasisgranger/Podcast.java
@@ -4,10 +4,8 @@ import java.util.Date;
 
 public class Podcast {
 	public Date publishedDate;
-	public Date updated;
-	public String link;
-	public String url;
 	public String title;
 	public String content;
-
+	public String link;
+	public String url;
 }
