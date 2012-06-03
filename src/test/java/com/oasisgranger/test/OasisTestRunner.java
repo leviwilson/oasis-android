ab38a314a04fea68e6aa67f347f39c3ebe5b6b0e
Levi Wilson <levi@leviwilson.com>
2012-06-02 20:39:51 -0400

    restructured project to be at the root

--- src/test/java/com/oasisgranger/test/OasisTestRunner.java
+++ src/test/java/com/oasisgranger/test/OasisTestRunner.java
@@ -14,7 +14,7 @@ import com.xtremelabs.robolectric.RobolectricTestRunner;
 public class OasisTestRunner extends RobolectricTestRunner {
 
 	public OasisTestRunner(Class<?> testClass) throws InitializationError {
-		super(testClass, new File("../app"));
+		super(testClass);
 	}
 	
 	@Override
