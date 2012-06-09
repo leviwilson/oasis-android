f99b507453d6be3de11bf4984f447dadc31a64e0
Levi Wilson <levi@leviwilson.com>
2012-06-09 13:09:27 -0400

    removed R.

--- src/main/java/com/oasisgranger/HomeActivity.java
+++ src/main/java/com/oasisgranger/HomeActivity.java
@@ -5,13 +5,14 @@ import static com.oasisgranger.helpers.ViewHelper.afterClicking;
 import android.os.Bundle;
 
 import com.oasisgranger.R.id;
+import com.oasisgranger.R.layout;
 
 public class HomeActivity extends OasisActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_home);
+		setContentView(layout.activity_home);
 		
 		afterClicking(this, id.home_btn_podcast).goTo(PodcastsActivity.class);
 		afterClicking(this, id.home_btn_aboutus).goTo(AboutActivity.class);
