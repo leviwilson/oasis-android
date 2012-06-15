be9fb103dfbb216f4702de322a75d0d8974a9d86
Levi Wilson <levi@leviwilson.com>
2012-06-15 16:03:55 -0400

    removed old maven src tree

--- src/main/java/com/oasisgranger/task/TaskRunner.java
+++
@@ -1,6 +0,0 @@
-package com.oasisgranger.task;
-
-public interface TaskRunner {
-
-	public abstract <Parameter, Result> void run(WorkItem<Parameter, Result> work, Parameter... result);
-}
--- src/main/java/com/oasisgranger/task/WorkItem.java
+++
@@ -1,7 +0,0 @@
-package com.oasisgranger.task;
-
-public interface WorkItem<Parameter, Result> {
-
-	Result doInBackground(Parameter...parameters);
-	void onPostExecute(Result result);
-}
