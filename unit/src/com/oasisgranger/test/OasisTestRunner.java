68e65b735bc948ce29fe1fe6f0151b9a2d496abc
Levi Wilson <levi@leviwilson.com>
2012-06-15 01:39:50 -0400

    missing Guice Context in OasisTestRunner

--- unit/src/com/oasisgranger/test/OasisTestRunner.java
+++ unit/src/com/oasisgranger/test/OasisTestRunner.java
@@ -6,9 +6,11 @@ import org.junit.runners.model.InitializationError;
 import org.mockito.MockitoAnnotations;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.oasisgranger.OasisGrangerApp;
 import com.oasisgranger.task.TaskRunner;
+import com.xtremelabs.robolectric.Robolectric;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
 
 public class OasisTestRunner extends RobolectricTestRunner {
@@ -28,6 +30,7 @@ public class OasisTestRunner extends RobolectricTestRunner {
 	protected Application createApplication() {
 		final OasisGrangerApp application =  new OasisGrangerApp();
 		application.configure()
+			.withBinding(Context.class, Robolectric.application)
 			.withBinding(TaskRunner.class, InlineTaskRunner.class);
 		return application;
 	}
