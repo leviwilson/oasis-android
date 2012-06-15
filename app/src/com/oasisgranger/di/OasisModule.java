85b4b07e7b5c905c1bc142361a11331afd77643b
Levi Wilson <levi@leviwilson.com>
2012-06-15 01:34:37 -0400

    missing Guice bindings

--- app/src/com/oasisgranger/di/OasisModule.java
+++ app/src/com/oasisgranger/di/OasisModule.java
@@ -1,11 +1,14 @@
 package com.oasisgranger.di;
 
+import android.media.MediaPlayer;
+
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.oasisgranger.DialogFacade;
 import com.oasisgranger.OasisPodcasts;
 import com.oasisgranger.Requestor;
 import com.oasisgranger.media.PodcastPlayer;
+import com.oasisgranger.media.PodcastServiceConnection;
 import com.oasisgranger.task.AsyncTaskRunner;
 import com.oasisgranger.task.TaskRunner;
 
@@ -19,7 +22,9 @@ public class OasisModule extends AbstractModule {
 
 	private void bindProduction() {
 		bind(DialogFacade.class).to(DialogFactory.class);
+		bind(MediaPlayer.class);
 		bind(PodcastPlayer.class);
+		bind(PodcastServiceConnection.class);
 		bind(Requestor.class).to(HttpRequestor.class);
 		bind(TaskRunner.class).to(AsyncTaskRunner.class);
 	}
