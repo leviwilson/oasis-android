ace324502fb2fcc58354cfc64323241f429f5fd9
Levi Wilson <levi@leviwilson.com>
2012-06-21 18:34:52 -0400

    cleaned up test output warnings

--- unit/src/com/oasisgranger/OasisActivityTest.java
+++ unit/src/com/oasisgranger/OasisActivityTest.java
@@ -8,30 +8,32 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 
+import android.os.Bundle;
+
+import com.oasisgranger.R.layout;
 import com.oasisgranger.logging.FlurryLogger;
 import com.oasisgranger.test.OasisTestRunner;
 import com.xtremelabs.robolectric.Robolectric;
 
 @RunWith(OasisTestRunner.class)
 public class OasisActivityTest {
-	
+
 	@Mock FlurryLogger flurryLogger;
-	
-	private OasisActivity activity;
-	
+	private OasisTestActivity activity;
+
 	@Before
 	public void setUp() {
 		setupMocks();
-		
-		activity = new OasisActivity();
+
+		activity = new OasisTestActivity();
 		shadowOf(activity).create();
 	}
-	
+
 	@Test
 	public void itInitiatesAFlurrySession() {
 		verify(flurryLogger).startSession(activity);
 	}
-	
+
 	@Test
 	public void itFinalizesTheFlurrySession() {
 		activity.onStop();
@@ -42,6 +44,15 @@ public class OasisActivityTest {
 		OasisGrangerApp application = (OasisGrangerApp) Robolectric.application;
 		application.configure()
 			.withBinding(FlurryLogger.class, flurryLogger);
+	}
+
+	private class OasisTestActivity extends OasisActivity {
+
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			setContentView(layout.activity_home);
+			super.onCreate(savedInstanceState);
+		}
 	}
 
 }
